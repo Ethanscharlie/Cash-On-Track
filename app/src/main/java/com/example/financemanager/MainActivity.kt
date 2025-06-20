4f02e92687de1e1cfb85e15af8d55914e12f2f35
Ethanscharlie <ethanscharlie@protonmail.com>
2025-06-20 13:01:30 -0600

    Can navigate between screens

--- app/src/main/java/com/example/financemanager/MainActivity.kt
+++ app/src/main/java/com/example/financemanager/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -17,11 +18,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.financemanager.ui.theme.FinanceManagerTheme
 import model.Tracker
 import model.Record
 import model.Database
 
+enum class Screen(@StringRes val title: Int) {
+    Balace(title = 0),
+    Record(title = 1)
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,30 +48,68 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FinanceManagerTheme {
-                Column (Modifier.padding(40.dp)) {
-                    var trackedValue by remember { mutableStateOf("") }
-                    trackedValue = Tracker.getBalanceOfTracker("MainTracker").toString();
-
-                    var text by remember { mutableStateOf("") }
-
-                    OutlinedTextField (
-                        value = text,
-                        onValueChange = { text = it },
-                        label = { Text("Label") },
-                        maxLines = 1,
-                    )
-
-                    Button(onClick = {
-                        Record.addRecord(text.toDouble(), "MainTracker");
-                        trackedValue = Tracker.getBalanceOfTracker("MainTracker").toString();
-                    }) {
-                        Text(text = "Click Me")
-                    }
-
-                    Text(trackedValue)
-                }
+                CashTrackerApp()
             }
         }
+    }
+}
+
+@Composable
+fun CashTrackerApp(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Balace.name,
+        modifier = Modifier.padding(40.dp)
+    ) {
+        composable(route = Screen.Balace.name) {
+            BalanceView(navController)
+        }
+
+        composable(route = Screen.Record.name) {
+            RecordEntry(navController)
+        }
+    }
+}
+
+@Composable
+fun BalanceView(
+    navController: NavHostController
+) {
+    Column (Modifier.padding(40.dp)) {
+        Text(Tracker.getBalanceOfTracker("MainTracker").toString())
+
+        Button(onClick = {
+            navController.navigate(Screen.Record.name)
+        }) {
+            Text(text = "Add Record")
+        }
+    }
+}
+
+@Composable
+fun RecordEntry(
+    navController: NavHostController
+) {
+    Column (Modifier.padding(40.dp)) {
+        var text by remember { mutableStateOf("") }
+
+        OutlinedTextField (
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Label") },
+            maxLines = 1,
+        )
+
+        Button(onClick = {
+            Record.addRecord(text.toDouble(), "MainTracker");
+            navController.popBackStack()
+//            trackedValue = Tracker.getBalanceOfTracker("MainTracker").toString();
+        }) {
+            Text(text = "Click Me")
+        }
+
     }
 }
 
