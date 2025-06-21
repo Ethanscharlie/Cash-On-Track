3e2b7a27d3d2bf2d19c8c54037567b898de233ea
Ethanscharlie <ethanscharlie@protonmail.com>
2025-06-20 23:57:42 -0600

    Top bar and add button in corner

--- app/src/main/java/com/example/financemanager/CashTrackerApp.kt
+++ app/src/main/java/com/example/financemanager/CashTrackerApp.kt
@@ -1,6 +1,17 @@
 package com.example.financemanager
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -9,14 +20,46 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CashTrackerApp(
     navController: NavHostController = rememberNavController()
 ) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+
+                title = {
+                    Text("Top app bar")
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton (onClick = {
+                navController.navigate(Screen.Record.name)
+            }) {
+                Icon(Icons.Rounded.Add, contentDescription = "Add")
+            }
+        }
+    ) { innerPadding ->
+        Navigation(navController, innerPadding)
+    }
+}
+
+@Composable
+fun Navigation(
+    navController: NavHostController,
+    innerPadding: PaddingValues
+) {
     NavHost(
         navController = navController,
         startDestination = Screen.Balace.name,
-        modifier = Modifier.padding(40.dp)
+        modifier = Modifier
+            .padding(innerPadding)
     ) {
         composable(route = Screen.Balace.name) {
             BalanceView(navController)
@@ -35,4 +78,3 @@ fun CashTrackerApp(
         }
     }
 }
-
