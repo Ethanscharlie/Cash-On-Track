80748ceff7baa05e32d24d89de866b8f546021d9
Ethanscharlie <ethanscharlie@protonmail.com>
2025-06-21 17:59:40 -0600

    Added scaffold to Tracker add

--- app/src/main/java/com/example/financemanager/TrackerAddView.kt
+++ app/src/main/java/com/example/financemanager/TrackerAddView.kt
@@ -1,21 +1,31 @@
 package com.example.financemanager
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.unit.dp
-import androidx.test.services.storage.file.PropertyFile.Column
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -31,8 +41,45 @@ import androidx.navigation.NavHostController
 import model.Record
 import model.Tracker
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TrackerAdd(
+    navController: NavHostController
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+
+                title = {
+                    Text("Add Tracker")
+                },
+
+                navigationIcon = {
+                    TextButton (onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        },
+    ) { innerPadding ->
+        Column (
+            Modifier.padding(40.dp).padding(innerPadding)
+        ) {
+            MainAddFieldEntry(navController)
+        }
+
+    }
+}
+
+@Composable
+fun MainAddFieldEntry(
     navController: NavHostController
 ) {
     Column (Modifier.padding(40.dp)) {
